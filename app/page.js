1e0e7144b2923b847eb3f0c954385ba8ea85a135
Tatsupopopo <yotusima@gmail.com>
2025-11-27 19:02:41 +0900

    Ch.5 サーバーからデータを取得する

--- app/page.js
+++ app/page.js
@@ -1,19 +1,47 @@
+"use client";
+
 import Image from "next/image";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Script from "next/script";
 import Link from "next/link";
 import Layout from "./components/layout";
 import MyImage from "./components/image";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  const title = "Index";
+  const [data, setData] = useState({ message: "", data: [] });
+
+  // 今回はクライアントサイドで動くため、useEffectが必要.
+  useEffect(() => {
+    fetch("/data.json")
+      .then((res) => res.json())
+      .then((res) => setData(res))
+      .catch((err) => console.error("Failed to fetch:", err));
+  }, []);
 
   return (
     <div>
       <Layout header="Next.js" title="Top Page">
         <div className="alert alert-primary text-center">
-          <h5 className="mb-4">Welcome to next.js!</h5>
-          <MyImage fname="image.jpg" size="300" />
+          <h5 className="mb-4">{data.message}</h5>
+          <table className="table bg-white">
+            <thead className="table-dark">
+              <tr>
+                <th>Name</th>
+                <th>Mail</th>
+                <th>Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.data.map((value, key) => (
+                <tr key={key}>
+                  <th>{value.name}</th>
+                  <td>{value.mail}</td>
+                  <td>{value.age}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </Layout>
       <Script
